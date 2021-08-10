c4edc8ffc2304dd27a65ec2186747c6e4335e9ed
html3012 <b_dana@ukr.net>
2021-08-10 20:45:15 +0300

    added new files and chenged

---
+++ app/js/main.js
@@ -0,0 +1,140 @@
+$(function () {
+    $('.slider-message__inner').slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        arrows: true,
+        fade: true,
+        autoplay: true,
+        responsive: [
+            {
+                breakpoint: 991.90,
+                settings: {
+                    dots: true,
+                    arrows: false,
+                }
+            }
+        ]
+    });
+
+    $('.brends__items').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 991.90,
+                settings: {
+                    slidesToShow: 5,
+                }
+            },
+            {
+                breakpoint: 767.90,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 479.90,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+        ]
+    });
+
+    $('.btn-burger').on('click', function () {
+        $('.btn-burger__line').addClass('btn-burger__line--active');
+        $('.menu').addClass('menu--active');
+        $('body').addClass('lock');
+    });
+
+    $('.menu__close').on('click', function () {
+        $('.btn-burger__line').removeClass('btn-burger__line--active');
+        $('.menu').removeClass('menu--active');
+        $('body').removeClass('lock');
+    });
+
+    $('.header__user-link--search').on('click', function () {
+        $('.header__search').toggleClass('header__search--active');
+    });
+
+    $('.dropdown').on('click', function () {
+        $('.dropdown').toggleClass('dropdown--active');
+    });
+
+    $('.filter__name').on('click', function () {
+        if ($(this).parent().hasClass('filter__categories--open')){
+            $(this).parent().removeClass('filter__categories--open')
+        } else {
+            $(this).parent().addClass('filter__categories--open')
+        }
+
+    });
+   
+    $('.header__user-link--cart').on('click', function () {
+        $('.basket').addClass('basket--active');
+        $('.header').addClass('header--active');
+    });
+
+    $('.basket__exit').on('click', function () {
+        $('.basket').removeClass('basket--active');
+        $('.header').removeClass('header--active');
+    }); 
+
+    $('.btn-grid').on('click', function () {
+        $('.btn-list').removeClass('selects__btn--active');
+        $(this).addClass('selects__btn--active');
+        $('.catalog__content--list').addClass('catalog__content--hidden');
+        $('.catalog__content--grid').removeClass('catalog__content--hidden');
+    });
+
+    $('.btn-list').on('click', function() {
+        $('.btn-grid').removeClass('selects__btn--active');
+        $(this).addClass('selects__btn--active');
+        $('.catalog__content--list').removeClass('catalog__content--hidden');
+        $('.catalog__content--grid').addClass('catalog__content--hidden');
+    });
+
+    $('.selects__btn--filter').on('click', function () {
+        $('.catalog__filters').addClass('catalog__filters--active');
+        $('.header').addClass('header--active');
+    });
+
+    $('.catalog__close').on('click', function () {
+        $('.catalog__filters').removeClass('catalog__filters--active');
+        $('.header').removeClass('header--active');
+    }); 
+
+    $('.filter__range-slider').ionRangeSlider({
+        type: "double",
+
+        onStart: function(data) {
+            $('.filter__from').prop("value", data.from);
+            $('.filter__to').prop("value", data.to);
+           
+        },
+        
+        onChange: function(data) {
+            $('.filter__from').prop("value", data.from);
+            $('.filter__to').prop("value", data.to);
+        },
+    });
+
+    $('.selects__select').styler();
+
+    let containerEl1 = document.querySelector('[data-ref="container-1"]');
+    let containerEl2 = document.querySelector('[data-ref="container-2"]');
+   
+    let config = {
+        controls: {
+            scope: 'local'
+        }
+    };
+
+    let mixer1 = mixitup(containerEl1, config);
+    let mixer2 = mixitup(containerEl2, config);
+
+});
---
+++ gulpfile.js
@@ -0,0 +1,136 @@
+const { src, dest, parallel, series, watch } = require('gulp');
+const browserSync = require('browser-sync').create();
+const scss = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const autoPrefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+const cheerio = require('gulp-cheerio');
+const replace = require('gulp-replace');
+const sprite = require('gulp-svg-sprite');
+const ssi    = require('browsersync-ssi');
+
+function browsersync() {
+    browserSync.init({
+        server: {
+            baseDir: 'app/',
+        middleware: ssi({
+            baseDir: 'app/',
+            ext: '.html',
+          })
+        },
+        notify: false,
+        online: true,
+    });
+}
+
+function styles() {
+    return src([
+        'node_modules/slick-carousel/slick/slick.scss',
+        'node_modules/ion-rangeslider/css/ion.rangeSlider.css',
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.css',
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.theme.css',
+        'app/scss/style.scss'
+       
+    ])
+        .pipe(scss({ outputStyle: 'compressed' }))
+        .pipe(concat('style.min.css'))
+        .pipe(autoPrefixer({
+            overrideBrowserslist: ['last 10 versions'],
+            grid: true
+        }))
+        .pipe(dest('app/css'))
+        .pipe(browserSync.stream())
+}
+
+function scripts() {
+    return src([
+        'node_modules/jquery/dist/jquery.js',
+        'node_modules/slick-carousel/slick/slick.js',
+        'node_modules/mixitup/dist/mixitup.js',
+        'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
+        'app/js/main.js',
+    ])
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(dest('app/js'))
+        .pipe(browserSync.stream())
+}
+
+function images() {
+    return src('app/img/**/*.*')
+        .pipe(imagemin(
+            [
+                imagemin.gifsicle({ interlaced: true }),
+                imagemin.mozjpeg({ quality: 75, progressive: true }),
+                imagemin.optipng({ optimizationLevel: 5 }),
+                imagemin.svgo({
+                    plugins: [
+                        { removeViewBox: true },
+                        { cleanupIDs: false }
+                    ]
+                })
+            ]
+        ))
+        .pipe(dest('dist/img'))
+}
+
+function svgSprite() {
+    return src('app/img/sprite/*.svg')
+        .pipe(cheerio({
+            run: function ($) {
+                $('[fill]').removeAttr('fill');
+                $('[stroke]').removeAttr('stroke');
+                $('[style]').removeAttr('style');
+            },
+            parserOptions: {
+                xmlMode: true,
+            }
+        }))
+        .pipe(replace('&gt;','>'))
+        .pipe(sprite({
+            mode: {
+                stack: {
+                    sprite:'../sprite.svg'
+                }
+            }
+        }))
+        .pipe(dest('app/img'));
+}
+
+
+function build() {
+    return src([
+        'app/css/style.min.css',
+        'app/js/main.min.js',
+        'app/**/*.html',
+        'app/fonts/**/*',
+    ], { base: 'app' })
+        .pipe(dest('dist'))
+}
+
+function cleanDist() {
+    return del('dist')
+}
+
+function watching() {
+    watch(['app/scss/**/*.scss'], styles);
+    watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
+    watch(['app/**/*.html']).on('change', browserSync.reload);
+    watch(['app/img/sprite/*.svg'], svgSprite);
+}
+
+
+exports.watching = watching;
+exports.styles = styles;
+exports.browsersync = browsersync;
+exports.scripts = scripts;
+exports.images = images;
+exports.cleandist = cleanDist;
+exports.svgSprite = svgSprite;
+
+exports.build = series(cleanDist, images, build);
+
+exports.default = parallel(styles, scripts, svgSprite, browsersync, watching);
